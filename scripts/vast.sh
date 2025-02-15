b51cc0e1f8edc75fee6a81825394e104f7d0673e
Constantin Müller <info@mueller-constantin.de>
2025-02-15 10:53:33 +0100

    fix: ensure nano and git is installed when using vast.ai

--- scripts/vast.sh
+++ scripts/vast.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+apt-get update && apt-get install -y git
+apt-get update && apt-get install -y nano
+
 git config --local user.name "Constantin Müller"
 git config --local user.email "info@mueller-constantin.de"
 
